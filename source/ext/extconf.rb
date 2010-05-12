fff072c2b992fffd40caffedad8816f435988c8e
kaazoo <kaazoo@drqueue.org>
2010-05-12 07:45:30 +0200

    use Git instead of SVN & fix paths & build only libdrqueue

--- source/ext/extconf.rb
+++ source/ext/extconf.rb
@@ -1,9 +1,9 @@
 require 'mkmf'
 require 'ftools'
 
-# check if Subversion, SWIG and SCons are installed
-if xsystem('which svn') == false
-  puts 'Subversion is not installed'
+# check if Git, SWIG and SCons are installed
+if xsystem('which git') == false
+  puts 'Git is not installed'
   exit 1
 end
 if xsystem('which swig') == false
@@ -29,33 +29,33 @@ elsif RUBY_PLATFORM =~ /irix6/i
 end
 
 # try to do a SVN checkout
-if xsystem('svn co --non-interactive https://ssl.drqueue.org/svn/branches/0.64.x') == false
-  puts 'SVN checkout failed'
+if xsystem('git clone https://ssl.drqueue.org/git/drqueue.git') == false
+  puts 'Git clone failed'
   exit 1
 end
 
-# try to build drqueue
-Dir::chdir '0.64.x' 
-if xsystem('scons build_drqman=false') == false
-  puts 'DrQueue build failed'
+# try to build libdrqueue
+Dir::chdir 'drqueue' 
+if xsystem('scons libdrqueue') == false
+  puts 'libdrqueue build failed'
   exit 1
 end
 Dir::chdir '..'
 
 # create swig wrapper
 puts 'generating swig interface file'
-puts 'swig -ruby -I0.64.x/ -I0.64.x/libdrqueue -D'+rb_os+' -Wall -autorename libdrqueue_ruby.i'
-xsystem('swig -ruby -I0.64.x/ -I0.64.x/libdrqueue -D'+rb_os+' -Wall -autorename libdrqueue_ruby.i')
+puts 'swig -ruby -Idrqueue/ -Idrqueue/libdrqueue -D'+rb_os+' -Wall -autorename libdrqueue_ruby.i'
+xsystem('swig -ruby -Idrqueue/ -Idrqueue/libdrqueue -D'+rb_os+' -Wall -autorename libdrqueue_ruby.i')
 puts 'look for output in mkmf.log'
 
 # build for os
 $CFLAGS += ' -D'+rb_os
 
 # include the headers
-$CFLAGS += ' -I0.64.x/ -I0.64.x/libdrqueue'
+$CFLAGS += ' -Idrqueue/ -Idrqueue/libdrqueue'
 
 # path to the drqueue lib
-$LOCAL_LIBS += '0.64.x/libdrqueue/libdrqueue.a'
+$LOCAL_LIBS += 'drqueue/libdrqueue/libdrqueue.a'
 
 # create the makefile
 create_makefile('drqueue')
